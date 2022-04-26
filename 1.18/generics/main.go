26abc4b04ab353dff4049b3504ea83b6594377e8
hiroto.ohira <hiroto100114@gmail.com>
2022-04-26 14:37:21 +0900

    Add include demo.

--- 1.18/generics/main.go
+++ 1.18/generics/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Printf("generics demo.\n")
+    compare()
+}
+
+func compare() {
+    // true
+    fmt.Println(include([]string{"foo", "bar"}, "bar"))
+
+    // false
+    fmt.Println(include([]string{"foo", "bar"}, "baz"))
+}
+
+func include[T comparable](list []T, value T) bool {
+    for _, element := range list {
+        if element == value {
+            return true
+        }
+    }
+
+    return false
 }
