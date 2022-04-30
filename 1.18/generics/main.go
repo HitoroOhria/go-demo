d4f85396e8740a7c5d1db6a956d9e9bbc3f129b4
hiroto.ohira <hiroto100114@gmail.com>
2022-04-30 20:46:31 +0900

    write include for value object.

--- 1.18/generics/main.go
+++ 1.18/generics/main.go
@@ -3,23 +3,47 @@ package main
 import "fmt"
 
 func main() {
-    compare()
+	compare()
 }
 
 func compare() {
-    // true
-    fmt.Println(include([]string{"foo", "bar"}, "bar"))
+	// true
+	fmt.Println(include([]string{"foo", "bar"}, "bar"))
 
-    // false
-    fmt.Println(include([]string{"foo", "bar"}, "baz"))
+	// false
+	fmt.Println(include([]string{"foo", "bar"}, "baz"))
+
+	// values := []*ValueObject{
+	// 	&ValueObject{Value: "foo"},
+	// 	&ValueObject{Value: "bar"},
+	// 	&ValueObject{Value: "baz"},
+	// }
+	//
+	// fmt.Println(values, &ValueObject{Value: "foo"})
 }
 
 func include[T comparable](list []T, value T) bool {
-    for _, element := range list {
-        if element == value {
-            return true
-        }
-    }
+	for _, element := range list {
+		if element == value {
+			return true
+		}
+	}
 
-    return false
+	return false
+}
+
+type ValueObject[T comparable] struct {
+	Value T
+}
+
+// WIP interface is (or embeds) comparable
+func includeForValueObj[T ValueObject[comparable]](list []T, value T) bool {
+	for _, element := range list {
+		// WIP undefined (type T has no field or method Value)[comparable]
+		if element.Value == value.Value {
+			return true
+		}
+	}
+
+	return false
 }
